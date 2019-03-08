e356634914bae9a2e1486928c0e23b78801ef2b4
PCK11800 <32240017+PCK11800@users.noreply.github.com>
2019-03-08 10:19:41 +0000

    Add files via upload

--- Pieces.java
+++ Pieces.java
@@ -8,6 +8,7 @@ public class Pieces extends Board{
     ImageIcon white = new ImageIcon("white.png");
     JButton placeHolder = new JButton();
     ImageIcon yellow = new ImageIcon("selected.png");
+    JButton cancelButton = new JButton("Cancel!");
     int clickCount = 0;
 
 
@@ -45,10 +46,22 @@ public class Pieces extends Board{
                 if(me.getButton() == MouseEvent.BUTTON3){
                     if(me.getClickCount() == 1){
                         confirmDeletelabel.setText("You sure you want to delete this? Double Right Click again to proceed.");
-                        piece.setBorder(BorderFactory.createLineBorder(Color.red, 5));
+                        piece.setBorder(BorderFactory.createLineBorder(Color.red, 2));
+                        cancelButton.setBounds(10, (640 + 20), 50, 20);
+                        deleteConfirm.add(cancelButton);
+                        cancelButton.addActionListener(new ActionListener(){
+                           public void actionPerformed(ActionEvent e){
+                               piece.setBorderPainted(false);
+                               confirmDeletelabel.setText("");
+                               deleteConfirm.remove(cancelButton);
+                               deleteConfirm.repaint();
+                           } 
+                        });
                     }
                     else if(me.getClickCount() == 2){
                         confirmDeletelabel.setText("");
+                        deleteConfirm.remove(cancelButton);
+                        deleteConfirm.repaint();
                         boardPane.remove(piece);
                     }
                 }
